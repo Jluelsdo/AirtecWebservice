5481fd4b5e84e1759dae3996fdee7af181a7cbd8
Jonas Lülsdorf <jonas.luelsdorf@th-koeln.de>
2023-09-01 12:37:53 +0200

    Implement create_patient, home and a base html menu

--- airtecWebservice/patients/urls.py
+++ airtecWebservice/patients/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 
-from .views import HomeView
+from .views import HomeView, CreatePatientView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('patients/', CreatePatientView.as_view(), name='create_patient')
 ]
