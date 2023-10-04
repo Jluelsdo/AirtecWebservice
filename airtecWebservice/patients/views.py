c9d16df83f984c33ee6593b185fcf4c737f4d317
Jonas Lülsdorf <jonas.luelsdorf@th-koeln.de>
2023-10-04 16:19:51 +0200

    Forward from listview to detailview, add a searchfield

--- airtecWebservice/patients/views.py
+++ airtecWebservice/patients/views.py
@@ -13,7 +13,9 @@ class CreatePatientView(CreateView):
     template_name = 'patients/create_patient.html'
     success_url = '/patients/'
     model = Patient
-    fields = ['patient_id', 'größe', 'gewicht', 'geschlecht', 'alter', 'andere_informationen', 'gesichtstyp', 'prothesenträger', 'prothese', 'abdruck_zeitpunkt', 'abdruck_ort']
+    fields = ['patient_id', 'größe', 'gewicht', 'geschlecht', 'alter',
+              'andere_informationen', 'gesichtstyp', 'prothesenträger',
+              'prothese', 'abdruck_zeitpunkt', 'abdruck_ort']
 
     def form_valid(self, form):
         form.instance.created_by = self.request.user
@@ -25,6 +27,14 @@ class ListPatientView(ListView):
     model = Patient
     context_object_name = 'patients'
     ordering = ['patient_id']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_query = self.request.GET.get('q')
+        if search_query:
+            queryset = queryset.filter(patient_id__icontains=search_query)
+        return queryset
+
 class DetailPatientView(DetailView):
     """Display details of a patient."""
     template_name = 'patients/detail.html'
