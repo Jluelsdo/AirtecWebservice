c272b2e3f640d6ca5f08b154a1962dddbebd3ed7
Jonas Lülsdorf <jonas.luelsdorf@th-koeln.de>
2023-12-06 11:32:40 +0100

    Add mask detail and mask list restapi

--- airtecWebservice/restapi/urls.py
+++ airtecWebservice/restapi/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from .views import PatientsViewSet, PatientDetailView
+from .views import PatientsViewSet, PatientDetailView, MaskeViewSet, MaskeDetailView
 
 urlpatterns = [
     path('patients/', PatientsViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('patients/<int:patient_id>/', PatientDetailView.as_view()),
+    path('maske/', MaskeViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('maske/<int:id>/', MaskeDetailView.as_view()),
     path('api-auth/', include('rest_framework.urls'))
     ]
--- airtecWebservice/restapi/views.py
+++ airtecWebservice/restapi/views.py
@@ -4,8 +4,8 @@ from django.http import Http404
 from rest_framework import viewsets, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import PatientSerializer
-from patients.models import Patient
+from .serializers import PatientSerializer, MaskeSerializer
+from patients.models import Patient, Maske
 
 
 class PatientsViewSet(viewsets.ModelViewSet):
@@ -99,4 +99,97 @@ class PatientDetailView(APIView):
         """
         patient = self.get_object(patient_id)
         patient.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class MaskeViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for handling mask data.
+    """
+    queryset = Maske.objects.all()
+    serializer_class = MaskeSerializer
+
+class MaskeDetailView(APIView):
+    """
+    A view for retrieving, updating, and deleting a specific mask.
+
+    Methods:
+    - get_queryset: Returns the queryset of all masks.
+    - get_object: Returns the mask object with the specified id.
+    - get: Retrieves the details of a specific mask.
+    - put: Updates the details of a specific mask.
+    - delete: Deletes a specific mask.
+    """
+
+    def get_queryset(self):
+        """
+        Returns the queryset of all masks.
+        """
+        return Maske.objects.all()
+
+    def get_object(self, id):
+        """
+        Returns the mask object with the specified id.
+
+        Args:
+        - id: The ID of the mask.
+
+        Raises:
+        - Http404: If the mask with the specified ID does not exist.
+        """
+        try:
+            return Maske.objects.get(id=id)
+        except Maske.DoesNotExist:
+            raise Http404
+
+    def get(self, request, id, format=None):
+        """
+        Retrieves the details of a specific mask.
+
+        Args:
+        - request: The HTTP request object.
+        - id: The ID of the mask to retrieve.
+        - format: The format of the response data (default: None).
+
+        Returns:
+        - Response: The serialized data of the mask.
+        """
+        mask = self.get_object(id)
+        serializer = MaskeSerializer(mask)
+        return Response(serializer.data)
+
+    def put(self, request, id, format=None):
+        """
+        Updates the details of a specific mask.
+
+        Args:
+        - request: The HTTP request object.
+        - id: The ID of the mask to update.
+        - format: The format of the request data (default: None).
+
+        Returns:
+        - Response: The serialized data of the updated mask if the request data is valid,
+                    otherwise the errors in the serializer.
+        """
+        mask = self.get_object(id)
+        serializer = MaskeSerializer(mask, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    def delete(self, request, id, format=None):
+        """
+        Deletes a specific mask.
+
+        Args:
+        - request: The HTTP request object.
+        - id: The ID of the mask to delete.
+        - format: The format of the response data (default: None).
+
+        Returns:
+        - Response: The HTTP response with status code 204 (No Content).
+        """
+        mask = self.get_object(id)
+        mask.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
